212196335e338a1f39e7c2061ee49c3e8475a561
Batzy <kannanvinodh19@gmail.com>
2025-06-16 01:25:16 +0800

    Vite  reconfigured

--- SquidGameCollins.Client/vite.config.js
+++ SquidGameCollins.Client/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
+
 import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
@@ -25,18 +26,12 @@ if (isDev) {
     const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
     const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-    if (!fs.existsSync(baseFolder)) {
-        fs.mkdirSync(baseFolder, { recursive: true });
-    }
-
     if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
         const result = child_process.spawnSync('dotnet', [
             'dev-certs',
             'https',
-            '--export-path',
-            certFilePath,
-            '--format',
-            'Pem',
+            '--export-path', certFilePath,
+            '--format', 'Pem',
             '--no-password',
         ], { stdio: 'inherit' });
 
@@ -52,20 +47,22 @@ if (isDev) {
 }
 
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [react()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-    server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false,
+    server: isDev
+        ? {
+            proxy: {
+                '^/weatherforecast': {
+                    target,
+                    secure: false,
+                },
             },
-        },
-        port: parseInt(env.DEV_SERVER_PORT || '49619'),
-        https: httpsConfig,
-    },
+            port: parseInt(env.DEV_SERVER_PORT || '49619'),
+            https: httpsConfig,
+        }
+        : undefined, // No dev server config in Railway
 });
